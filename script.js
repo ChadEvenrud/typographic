4bb7a809308b284e3e8420a3db30c4b5b7f9e3e3
Chad Evenrud <87739913+ChadEvenrud@users.noreply.github.com>
2024-04-29 09:35:00 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const root = document.documentElement;
 const currentTheme = localStorage.getItem("theme");
 const nav = document.getElementsByTagName("nav")[0];
 const navIcon = document.getElementsByClassName("menu-icon")[0];
+const navItems = document.getElementsByClassName("menu-items")[0];
+
+
 
 //Functions
 
@@ -21,9 +24,12 @@ const switchTheme = () => {
 
 const activeNav = () => {
   if (nav.style.transform === "translateX(-100%)") {
+    nav.style.height="85vh";   
     nav.style.transform = "";
   } else {
     nav.style.transform = "translateX(-100%)";
+    navItems.style.height = "0px";
+    nav.style.height = "0px";
   }
 };
 
@@ -31,6 +37,11 @@ const activeNav = () => {
 
 themeSwitcher.addEventListener("click", switchTheme);
 navIcon.addEventListener("click", activeNav);
+nav.addEventListener("click", activeNav)
+
+for (let i = 0; i < navIcon; i++) {
+  console.log(navIcon[i]);
+}
 
 // Check Local Storage For Theme
 
